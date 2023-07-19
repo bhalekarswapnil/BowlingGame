ca8061f6fbf6423b6fce75ee7baa3d4601eac0d9
bhalekarswapnil <85471791+bhalekarswapnil@users.noreply.github.com>
2023-07-19 09:54:08 +0530

    Update BowlingGame.java

--- src/main/java/org/example/BowlingGame.java
+++ src/main/java/org/example/BowlingGame.java
@@ -6,15 +6,26 @@ public class BowlingGame implements Game{
     private int i;
     @Override
     public void roll(int pins) {
-       hits[++i] = pins;
+       hits[i++] = pins;
     }
 
     @Override
     public int score() {
         int score = 0;
-
-        for(int i=1;i<21;i++){
-            score+=hits[i];
+        int roll = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            int frameScore = 0;
+            if (hits[roll] == 10) {
+                frameScore = 10 + hits[roll + 1] + hits[roll + 2];
+                roll++;
+            } else if ((hits[roll] + hits[roll + 1]) == 10) {
+                frameScore = 10 + hits[roll + 2];
+                roll = roll + 2;
+            } else {
+                frameScore = hits[roll] + hits[roll + 1];
+                roll = roll + 2;
+            }
+            score += frameScore;
         }
         return score;
     }
